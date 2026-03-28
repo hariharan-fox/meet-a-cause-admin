40e16466467452ac35e3c71c86096f4fc0e93db3
Fox Social <hellofoxsocial@gmail.com>
2026-03-28 15:19:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "just-hands-admin-5968075-3b61b",
+  "appId": "1:863197164269:web:8e228e733ff474856912e4",
+  "apiKey": "AIzaSyBe7Wf6-8htmk3gQxckw32IRYgU7XyK5QA",
+  "authDomain": "just-hands-admin-5968075-3b61b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "863197164269"
+};
