1237d1ced842bb91619eb310bd8b4d2df9b6a592
Fox Social <hellofoxsocial@gmail.com>
2026-03-28 15:43:58 +0000

    What happened?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9823971235-a7e78",
+  "projectId": "just-hands-admin-5968075-3b61b",
   "appId": "1:863197164269:web:8e228e733ff474856912e4",
   "apiKey": "AIzaSyBe7Wf6-8htmk3gQxckw32IRYgU7XyK5QA",
-  "authDomain": "studio-9823971235-a7e78.firebaseapp.com",
+  "authDomain": "just-hands-admin-5968075-3b61b.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "863197164269"
 };
